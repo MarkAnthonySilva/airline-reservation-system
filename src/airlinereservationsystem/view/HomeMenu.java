15372f8e8244c1b6f6be455bbef3b1472b624234
markanthonysilva <masilva1619@gmail.com>
2021-11-13 03:08:54 -0800

    Update HomeMenu.java

--- src/airlinereservationsystem/view/HomeMenu.java
+++ src/airlinereservationsystem/view/HomeMenu.java
@@ -7,7 +7,7 @@ public class HomeMenu {
 	public String displayHome() {
 		Console console = System.console();
 		if(console !=null){
-			System.out.println("Navigate to where");
+			System.out.println("\nHOME MENU\nNavigate to where");
 			System.out.println("1: Passenger");
 			String navIntAsString = console.readLine("Enter integer: ");
 			return navIntAsString;
