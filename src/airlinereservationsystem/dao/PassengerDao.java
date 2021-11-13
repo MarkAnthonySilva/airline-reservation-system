8a0b0deeb7e213d24c7023680920b3facf4851a0
markanthonysilva <masilva1619@gmail.com>
2021-11-13 09:39:38 -0800

    Get Passengers by name

--- src/airlinereservationsystem/dao/PassengerDao.java
+++ src/airlinereservationsystem/dao/PassengerDao.java
@@ -1,12 +1,15 @@
 package airlinereservationsystem.dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 import airlinereservationsystem.JdbcConnector;
 import airlinereservationsystem.model.Passenger;
 
 public class PassengerDao {
 	private final Connection CONNECTION = JdbcConnector.createNewConnection();
 	private final String INSERT_PASSENGER = "INSERT INTO passenger (firstName, lastName, age) VALUES (?, ?, ?)";
+	private final String SELECT_PASS_NAME = "SELECT * FROM passenger WHERE name = ?";
 	
 	public void insertPassenger(Passenger passenger) {
 		try {
@@ -20,5 +23,17 @@ public class PassengerDao {
 			System.out.println("Passenger Could not be added Added");
 			e.printStackTrace();
 		}
+	}
+	
+	public ResultSet selectPassengerName(String name) {
+		try {
+			PreparedStatement ps = this.CONNECTION.prepareStatement(this.SELECT_PASS_NAME);
+			ps.setString(1, name);
+			return ps.executeQuery();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return null;
 	}
 }
