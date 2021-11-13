ab429d71899fe884e68d3c0806e90f36614dd955
markanthonysilva <masilva1619@gmail.com>
2021-11-13 09:36:44 -0800

    Add other options to home menu

--- src/airlinereservationsystem/view/HomeMenu.java
+++ src/airlinereservationsystem/view/HomeMenu.java
@@ -12,6 +12,11 @@ public class HomeMenu {
 		System.out.println("\nHOME MENU\nNavigate to where");
 		System.out.println("0: Close Airline System");
 		System.out.println("1: Passenger");
+		System.out.println("2: Ticket");
+		System.out.println("3: Purchase");
+		System.out.println("4: Airline");
+		System.out.println("5: Blacklist");
+		System.out.println("6: Airport");
 		System.out.print("\nEnter integer: ");
 		String navIntAsString = sc.next();
 		return navIntAsString;
