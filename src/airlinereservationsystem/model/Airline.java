551ce3fcb55c9126ad85af6eab75fd60f2b11230
markanthonysilva <masilva1619@gmail.com>
2021-12-02 01:12:16 -0800

    Create Airline.java

---
+++ src/airlinereservationsystem/model/Airline.java
@@ -0,0 +1,54 @@
+package airlinereservationsystem.model;
+
+public class Airline {
+	private int aID;
+	private String name;
+	
+	public int getaID() {
+		return aID;
+	}
+	public void setaID(int aID) {
+		this.aID = aID;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + aID;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Airline other = (Airline) obj;
+		if (aID != other.aID)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+	
+	@Override
+	public String toString() {
+		return "Airline [aID=" + aID + ", name=" + name + "]";
+	}
+	
+	
+}
