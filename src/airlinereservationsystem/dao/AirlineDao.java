c25854d69a74085925eb2dca01ce6881bb16a1ae
markanthonysilva <masilva1619@gmail.com>
2021-12-03 07:32:48 -0800

    Insert Airline Dao

--- src/airlinereservationsystem/dao/AirlineDao.java
+++ src/airlinereservationsystem/dao/AirlineDao.java
@@ -9,10 +9,20 @@ import airlinereservationsystem.model.Airline;
 
 public class AirlineDao {
 	private final Connection CONNECTION = JdbcConnector.createNewConnection();
-	private final String INSERT_AIRLINE = "INSERT INTO a (firstName, lastName, age) VALUES (?, ?, ?)";
-	private final String SELECT_AIRLINE_NAME = "SELECT * FROM passenger WHERE firstName = ?";
-	private final String SELECT_AIRLINE_AID = "SELECT * FROM passenger WHERE pID = ?";
-	private final String DELETE_AIRLINE = "DELETE FROM passenger WHERE pID = ?";
+	private final String INSERT_AIRLINE = "INSERT INTO airline (name) VALUES (?)";
+//	private final String SELECT_AIRLINE_NAME = "SELECT * FROM passenger WHERE firstName = ?";
+//	private final String SELECT_AIRLINE_AID = "SELECT * FROM passenger WHERE pID = ?";
+//	private final String DELETE_AIRLINE = "DELETE FROM passenger WHERE pID = ?";
 	
-	
+	public boolean insertAirline(Airline a) {
+		try {
+			PreparedStatement ps = this.CONNECTION.prepareStatement(this.INSERT_AIRLINE);
+			ps.setString(1, a.getName());
+			ps.executeUpdate();
+			return true;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
 }
